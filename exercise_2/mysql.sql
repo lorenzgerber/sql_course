aa36d830580c9c01a4dc96a533f602fc76784709
Lorenz Gerber <dv15lgr@cs.umu.se>
2017-02-06 10:32:42 +0100

    set up exercise 2

---
+++ exercise_2/mysql.sql
@@ -0,0 +1,69 @@
+-- 1. Find the airport with the greatest latitude.
+-- In the case of a tie, list all such airports.
+SELECT ' Query 1 ';
+
+
+-- 2. Find the names of those airlines which do not
+-- have flights which depart from an airport in Germany.
+-- (This includes airlines which do not have any flights at all.)
+SELECT ' Query 2 ';
+
+
+
+-- 3. Find the names of those airlines which have a
+-- flight whose destination airport is the same as the origin
+-- airport of some flight of Lufthansa. (The query must use the
+-- string ’Lufthansa’ and not the airline abbreviation LH.)
+SELECT ' Query 3 ';
+
+-- 4. Find the names of those airports, all of whose departures
+-- are international, in the precise sense that the destination
+-- airport is in a different country than the airport of departure.
+-- Exclude airports with no departures.
+SELECT ' Query 4 ';
+
+-- 5. Find the names of those airlines which, for every airport
+-- in Germany with latitude less than 54, except possibly BER,
+-- have a flight whose origin is that airport.
+SELECT ' Query 5 ';
+
+-- 6. For each airport, find the total number of distinct destinations
+-- for flights which originate at that airport. List the airport code, city,
+-- and country, as well as the number of distinct destinations, and sort
+-- first by number of destinations and then by airport code, with the greatest
+-- number of airports first. Include even airports with no flights.
+-- (Hint: left outer join)
+SELECT ' Query 6 ';
+
+-- 7. For each country found in the Airport relation of the database,
+-- find the maximum, minimum, and average latitude over all airports which are
+-- located in that country, as well as the total number of airports
+-- for each such country.
+SELECT ' Query 7 ';
+
+
+-- 8. Find the code, city, and country of that airport which has the
+-- least positive number of distinct destinations for flights which
+-- originate at that airport. (That is, the airport must have at least
+-- one originating flight.) In case of a tie, list all such airports.
+-- Give the number of destinations for that airport as well.
+SELECT ' Query 8 ';
+
+-- 9. Find the sum of the ticket costs for each carrier for flights
+-- departing in the month of November 2016. Report 0 for those airlines
+-- with no ticket sales and order from highest to lowest. (Hint: The
+-- SQL directive ORDER BY n will order the result based upon the nth column.)
+SELECT ' Query 9 ';
+
+-- 10. Find the codes of those airports which are the origin for
+-- flights to at least three distinct airports in France.
+SELECT ' Query 10 ';
+
+-- 11. Find the names of those airlines which have both a flight
+-- with origin the airport with code TXL and a flight with origin
+-- the airport with code SXF.
+SELECT ' Query 11 ';
+
+-- 12. Find the codes of those airports located in Berlin,
+-- Germany which do not have any scheduled departures.
+SELECT ' Query 12 ';
--- start_mysql.sh
+++ start_mysql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysql -u v119v17dv15lgr -h mysql -p v119v17dv15lgr
+mysql -u v119v17dv15lgr -h mysql -pauR1ieV9 v119v17dv15lgr
